3c57b5ef74200ed1c488b2689cf1ba5156e8bebb
VishalMakwna08 <makwanavishal588@gmail.com>
2025-06-17 12:19:12 +0530

    This File Add Comments

--- B.c
+++ B.c
@@ -1,10 +1,12 @@
+//B Pattern C Program
 #include<stdio.h>
 
 int main(){
-  //outer loop
+  //outer for loop
     for(int i=1;i<=9;i++){
-        //inner loop
+        //inner for loop
         for(int j=1;j<=9;j++){
+            //conditional statement
             if(j==1||((i==1||i==9||i==5)&&(j<6)||(i>1&&i<5&&j==6))||(i>5&&i<9&&j==6)){
                 printf("* ");
             }else{
