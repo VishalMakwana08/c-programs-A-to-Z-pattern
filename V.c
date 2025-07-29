976e50707b935edd8a20680c084cfaa794afacbf
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-29 19:50:55 +0530

    V pattern C program

---
+++ V.c
@@ -0,0 +1,19 @@
+//V pattern c program
+#include<stdio.h>
+
+int main(){
+//outer for loop
+
+    for(int i = 1 ; i <= 9; i++){
+        //inner for loop
+        for(int j = 1; j <= 9; j++){
+            //conditional statement
+               if((i==j&&j<6)||(j==10-i&&j>5)){
+                    printf("*");
+                }else{
+                    printf(" ");
+                }
+        }printf("\n");
+    }
+    return 0;
+}
