d0861225adf9a07387115b43bafee0b2b352acdb
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-29 20:03:39 +0530

    Z pattern c program

---
+++ Z.c
@@ -0,0 +1,19 @@
+//Z pattern c program
+#include<stdio.h>
+
+int main(){
+//outer for loop
+
+    for(int i = 1 ; i <= 9; i++){
+        //inner for loop
+        for(int j = 1; j <= 9; j++){
+            //conditional statement
+               if((i==1||i==9)||(j==10-i)){
+                    printf("* ");
+                }else{
+                    printf("  ");
+                }
+        }printf("\n");
+    }
+    return 0;
+}
