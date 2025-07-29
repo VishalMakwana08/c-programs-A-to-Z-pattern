05e1137624f2acf97e30d604c95cbbe5079ba544
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-29 19:57:49 +0530

    X Pattern C program

---
+++ X.c
@@ -0,0 +1,19 @@
+//W pattern c program
+#include<stdio.h>
+
+int main(){
+//outer for loop
+
+    for(int i = 1 ; i <= 9; i++){
+        //inner for loop
+        for(int j = 1; j <= 9; j++){
+            //conditional statement
+               if((i==j)||(j==10-i)){
+                    printf("* ");
+                }else{
+                    printf("  ");
+                }
+        }printf("\n");
+    }
+    return 0;
+}
