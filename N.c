b158f30f5064b387e7b9e4cb9590bb4cee252ff4
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-29 19:04:25 +0530

    N pattern C Program

---
+++ N.c
@@ -0,0 +1,19 @@
+//N pattern c program
+#include<stdio.h>
+
+int main(){
+//outer for loop
+
+    for(int i = 1 ; i <= 9; i++){
+        //inner for loop
+        for(int j = 1; j <= 9; j++){
+            //conditional statement
+                if(j==1||j==9||i==j){
+                    printf("* ");
+                }else{
+                    printf("  ");
+                }
+        }printf("\n");
+    }
+    return 0;
+}
