dae1a491be2caacbc51c293da932cbf4be947b39
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-29 19:10:52 +0530

    R pattern C program

---
+++ R.C
@@ -0,0 +1,20 @@
+//R pattern c program
+#include<stdio.h>
+
+int main(){
+//outer for loop
+
+    for(int i = 1 ; i <= 11; i++){
+        //inner for loop
+        for(int j = 1; j <= 11; j++){
+            //conditional statement
+                if(j==1||(i==1&&j<8)||((i==2||i==5)&&j==8)||((i==3||i==4)&&j==9)||(i==6&&j<8)
+            ||(i>6&&j==i-2)){
+                    printf("* ");
+                }else{
+                    printf("  ");
+                }
+        }printf("\n");
+    }
+    return 0;
+}
